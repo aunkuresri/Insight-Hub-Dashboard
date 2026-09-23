00298c16cb23e209eb610157b9c97c0bb69a2310
aunkuresri <aunkur.esri@gmail.com>
2026-09-23 09:24:21 +0600

    Change icon for Crime KPI from 'hammer' to 'emergency'

--- src/components/analytics/map-kpi-bar.tsx
+++ src/components/analytics/map-kpi-bar.tsx
@@ -18,7 +18,7 @@ const INCIDENT_KPI_CARDS: Array<{
   {
     id: "Crime",
     label: "Crime",
-    icon: "hammer",
+    icon: "emergency",
     insight: "Reported criminal incidents",
     tone: "red",
   },
